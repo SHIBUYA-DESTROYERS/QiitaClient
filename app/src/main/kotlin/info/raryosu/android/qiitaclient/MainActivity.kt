350a3cab154e3386bfb1264a5e374ae2d109a9c9
raryosu <me@raryosu.info>
2017-11-03 14:33:45 +0900

    UIを途中まで

--- app/src/main/kotlin/info/raryosu/android/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/info/raryosu/android/qiitaclient/MainActivity.kt
@@ -2,11 +2,28 @@ package info.raryosu.android.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ListView
+import info.raryosu.android.qiitaclient.model.Article
+import info.raryosu.android.qiitaclient.model.User
+import info.raryosu.android.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "たろう"), dummyArticle("Java入門", "じろう"))
+
+        val listview: ListView = findViewById(R.id.list_view) as ListView
+        listview.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
---
+++ app/src/main/kotlin/info/raryosu/android/qiitaclient/extensions.kt
@@ -0,0 +1,8 @@
+package info.raryosu.android.qiitaclient
+
+import android.support.annotation.IdRes
+import android.view.View
+
+fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
+    findViewById<T>(id)
+}
