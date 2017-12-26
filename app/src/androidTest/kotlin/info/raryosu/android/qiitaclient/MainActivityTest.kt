30b2813a18b35f980ecbc840cc60bd8c23e86e51
raryosu <me@raryosu.info>
2017-12-26 23:47:29 +0900

    とりあえずテストを書いた
    - 日本語で書くのどうなんだ…

--- app/src/androidTest/kotlin/info/raryosu/android/qiitaclient/MainActivityTest.kt
+++ app/src/androidTest/kotlin/info/raryosu/android/qiitaclient/MainActivityTest.kt
@@ -1,14 +1,17 @@
 package info.raryosu.android.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.regex.Pattern.matches
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -17,7 +20,7 @@ class MainActivityTest {
     @Rule
     val activityTestRule = ActivityTestRule(MainActivity::class.java)
 
-    @test
+    @Test
     fun 各ビューが表示されていること_ただしプログレスバーは非表示() {
         onView(withId(R.id.list_view)).check(matches(isDisplayed()))
         onView(withId(R.id.query_edit_text)).check(matches(isDisplayed()))
@@ -25,4 +28,6 @@ class MainActivityTest {
 
         onView(withId(R.id.progress_bar)).check(matches(isNotDisplayed()))
     }
+
+    fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
 }
